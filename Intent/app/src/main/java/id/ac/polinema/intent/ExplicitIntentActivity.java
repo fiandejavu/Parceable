1fd84c91b299186722c2b49eb2d72ec8f9790cd2
fiandejavu <viandejavu@gmail.com>
2019-09-26 21:02:56 +0700

    Explicit dan Implicit Intent

--- Intent/app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
@@ -9,10 +9,18 @@ import android.widget.TextView;
 
 public class ExplicitIntentActivity extends AppCompatActivity {
 
+    private EditText input;
+    private TextView view_output;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_explicit_intent);
+        input = findViewById(R.id.input_name);
+        view_output = findViewById(R.id.text_output);
     }
 
+    public void handleSubmit(View view) {
+        view_output.setText("Nama : " +input.getText().toString());
+    }
 }
